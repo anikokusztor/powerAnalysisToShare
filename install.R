e9bb8654ab442a6f6ed22b4d654d60410ab84469
Aniko Kusztor <45171575+anikokusztor@users.noreply.github.com>
2024-10-28 19:30:14 +1100

    update

--- install.R
+++ install.R
@@ -1,3 +1,3 @@
 # install.R
-install.packages(c("ggplot2", "dplyr", "truncnorm", "ordinal", "IRkernel"))  # Add all required packages
-IRkernel::installspec(user = FALSE)  # Register the IRkernel for Jupyter
+install.packages(c("ggplot2", "dplyr", "truncnorm", "ordinal"))  # Add all required packages  "IRkernel"
+#IRkernel::installspec(user = FALSE)  # Register the IRkernel for Jupyter
